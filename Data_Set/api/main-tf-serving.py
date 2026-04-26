cd917e970c4df36448bb28a4bbb529bcc358a53e
Saud1304 <saud.sayyed.1301@gmail.com>
2026-04-26 13:22:19 +0530

    Initial commit

---
+++ Data_Set/api/main-tf-serving.py
@@ -0,0 +1,62 @@
+from http.client import responses
+
+from fastapi import FastAPI, UploadFile, File
+from io import BytesIO
+import numpy as np
+from PIL import Image
+import uvicorn
+from pygments.lexers import data
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+import tensorflow as tf
+from rich import json
+from starlette import endpoints
+
+#from tensorflow.python.grappler.item import Item
+#from tensorflow.python.keras.engine.training_v1 import Model
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+endpoint = "http://localhost:8501/v1/models/potatoes_model:predict"
+CLASS_NAMES = ["Early Blight","Late Blight", "Healthy"]
+@app.get("/")
+async def ping():
+    return "Hello"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)
+ ):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, axis=0)
+    json_data = {
+        "instances": img_batch.tolist()
+    }
+    response = requests.post(endpoint, json=json_data)
+    prediction = response.json()["predictions"][0]
+    predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    confidence = np.max(prediction)
+    return {
+        "class": predicted_class,
+        "confidence": float(confidence)
+    }
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
---
+++ Data_Set/api/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, UploadFile, File
+from io import BytesIO
+import numpy as np
+from PIL import Image
+import uvicorn
+from pygments.lexers import data
+import tensorflow as tf
+
+
+#from tensorflow.python.grappler.item import Item
+#from tensorflow.python.keras.engine.training_v1 import Model
+
+app = FastAPI()
+
+Model = tf.keras.models.load_model("../models/1")
+CLASS_NAMES = ["Early Blight","Late Blight", "Healthy"]
+@app.get("/")
+async def ping():
+    return "Hello"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)
+ ):
+    image = read_file_as_image(await file.read())
+
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = Model.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        "class": predicted_class,
+        "confidence": float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
