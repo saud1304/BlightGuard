36e6b22b12f660ffbc1267cda28d4ea748da0831
Saud Sayyed <86797433+saud1304@users.noreply.github.com>
2026-04-30 19:26:20 +0530

    Update main.py

--- Data_Set/api/main.py
+++ Data_Set/api/main.py
@@ -62,5 +62,5 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
